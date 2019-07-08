df1f37f6a873cc5f702d564ee04e8762ef759a9e
rkasaraneni <ramya_kasaraneni@intuit.com>
2019-07-08 10:00:58 -0700

    Removed Claims

--- OAuth2_CoreMVC_Sample/Controllers/ConnectController.cs
+++ OAuth2_CoreMVC_Sample/Controllers/ConnectController.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Intuit.Ipp.OAuth2PlatformClient;
+﻿using Intuit.Ipp.OAuth2PlatformClient;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
-using Microsoft.Extensions.Primitives;
 using OAuth2_CoreMVC_Sample.Helper;
 using OAuth2_CoreMVC_Sample.Models;
-
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace OAuth2_CoreMVC_Sample.Controllers
 {
@@ -42,9 +36,12 @@ namespace OAuth2_CoreMVC_Sample.Controllers
 
             var state = Request.Query["state"];
             string code = Request.Query["code"].ToString();
+            string realmId = Request.Query["realmId"].ToString();
+           
+
             if (state.Count>0 && !string.IsNullOrEmpty(code))
             {
-                await StoreClaims(state);
+                await GetAuthTokensAsync(code, realmId);
                 return RedirectToAction("Index", "QBO");
             }
             else
@@ -75,63 +72,19 @@ namespace OAuth2_CoreMVC_Sample.Controllers
                 return (View("Connect"));
             }
         }
-        private async Task StoreClaims(StringValues state)
-        {
-            oAuth2Client = new OAuth2Client(OAuth2Keys.ClientId, OAuth2Keys.ClientSecret, OAuth2Keys.RedirectUrl, OAuth2Keys.Environment);
-
-            if (state.ToString().Equals(oAuth2Client.CSRFToken, StringComparison.Ordinal))
-            {
-                ViewBag.State = state + " (valid)";
-            }
-            else
-            {
-                ViewBag.State = state + " (invalid)";
-            }
-
-            string code = Request.Query["code"].ToString() ?? "none";
-            string realmId = Request.Query["realmId"].ToString() ?? "none";
-            await GetAuthTokensAsync(code, realmId);
-
-            ViewBag.Error = Request.Query["error"].ToString() ?? "none";
-        }
 
         private async Task GetAuthTokensAsync(string code, string realmId)
+        {
+            oAuth2Client = new OAuth2Client(OAuth2Keys.ClientId, OAuth2Keys.ClientSecret, OAuth2Keys.RedirectUrl, OAuth2Keys.Environment);
+            var tokenResponse = await oAuth2Client.GetBearerTokenAsync(code);
+            OAuth2Keys.RealmId = realmId;
+            Token token = _tokens.Token.FirstOrDefault(t => t.RealmId == realmId);
+            if (token == null)
             {
-                if (realmId != null)
-                {
-                    TempData["realmId"] = realmId;
-                }
-
-                var tokenResponse = await oAuth2Client.GetBearerTokenAsync(code);
-
-
-                OAuth2Keys.RealmId = realmId;
-                var claims = new List<Claim>();
-
-                if (TempData["realmId"] != null)
-                {
-                    claims.Add(new Claim("realmId", TempData["realmId"].ToString()));
-                }
-
-                if (!string.IsNullOrWhiteSpace(tokenResponse.AccessToken))
-                {
-                    claims.Add(new Claim("access_token", tokenResponse.AccessToken));
-                    claims.Add(new Claim("access_token_expires_at", (DateTime.Now.AddSeconds(tokenResponse.AccessTokenExpiresIn)).ToString()));
-                }
-
-                if (!string.IsNullOrWhiteSpace(tokenResponse.RefreshToken))
-                {
-                    claims.Add(new Claim("refresh_token", tokenResponse.RefreshToken));
-                    claims.Add(new Claim("refresh_token_expires_at", (DateTime.Now.AddSeconds(tokenResponse.RefreshTokenExpiresIn)).ToString()));
-                }
-                var id = new ClaimsIdentity(claims, "Cookies");
-              Token token = _tokens.Token.FirstOrDefault(t => t.RealmId == realmId);
-                if (token == null)
-                {
-                    _tokens.Add(new Token { RealmId = realmId, AccessToken = tokenResponse.AccessToken, RefreshToken = tokenResponse.RefreshToken });
-                   await  _tokens.SaveChangesAsync();
-                }
+                _tokens.Add(new Token { RealmId = realmId, AccessToken = tokenResponse.AccessToken, RefreshToken = tokenResponse.RefreshToken });
+                await _tokens.SaveChangesAsync();
             }
+        }
      
     }
 }
